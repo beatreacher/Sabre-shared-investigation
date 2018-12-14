8f90dd38a3b6669fb8a246fad5d449df60433765
Alex.Kovalenko <alex.kovalenko@dataart.com>
2018-12-14 14:53:58 +0100

    TokenCreateRQ action added

--- SabreClientTest/AuthorizationTests.cs
+++ SabreClientTest/AuthorizationTests.cs
@@ -11,11 +11,17 @@ namespace SabreClientTest
     [TestClass]
     public class AuthorizationTests
     {
+        IDictionary<string, string> Actions = new Dictionary<string, string>
+        {
+            {"CreateToken", "TokenCreateRQ"},
+            {"CreateSession", "SessionCreateRQ"}
+        };
+
         [TestMethod]
         public async Task GetToken()
         {
             string url = @"https://sws3-crt.cert.sabre.com";
-            var sbb = GetRequest("7971", "WS102513", "92RG", "DEFAULT");
+            var sbb = GetRequest("7971", "WS102513", "92RG", "DEFAULT", "TokenCreateRQ");
             using (HttpClient client = new HttpClient { BaseAddress = new Uri(url) })
             {
                 client.DefaultRequestHeaders.Clear();
@@ -27,9 +33,13 @@ namespace SabreClientTest
                 var nsmgr = GetXmlNamespaceManager(doc);
                 string conversationId = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/eb:MessageHeader/eb:ConversationId").InnerText;
                 string token = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/wsse:Security/wsse:BinarySecurityToken").InnerText;
+                string messageId = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/eb:MessageHeader/eb:MessageData/eb:MessageId").InnerText;
+                string timeStamp = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/eb:MessageHeader/eb:MessageData/eb:Timestamp").InnerText;
 
                 Assert.IsNotNull(conversationId);
                 Assert.IsNotNull(token);
+                Assert.IsNotNull(messageId);
+                Assert.IsNotNull(timeStamp);
             }
         }
 
@@ -42,9 +52,13 @@ namespace SabreClientTest
             var nsmgr = GetXmlNamespaceManager(doc);
             string conversationId = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/eb:MessageHeader/eb:ConversationId").InnerText;
             string token = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/wsse:Security/wsse:BinarySecurityToken").InnerText;
+            string messageId = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/eb:MessageHeader/eb:MessageData/eb:MessageId").InnerText;
+            string timeStamp = GetNodeByPath(doc, nsmgr, "soap-env:Envelope/soap-env:Header/eb:MessageHeader/eb:MessageData/eb:Timestamp").InnerText;
 
             Assert.AreEqual("V1@280b16ec-5eac-46c0-893f-c88f8e8cb632@310b16ec-5dad-46c0-893f-c88f8e8cb643@780b16ec-5eac-46c0-893f-c88f8e8cb699", conversationId);
             Assert.AreEqual(@"Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTC!ICESMSLB\/CRT.LB!1544695565786!2375!17", token);
+            Assert.AreEqual("888281363657680150", messageId);
+            Assert.AreEqual("2018-12-13T10:06:05", timeStamp);
         }
 
         private static XmlNamespaceManager GetXmlNamespaceManager(XmlDocument doc)
@@ -71,7 +85,7 @@ namespace SabreClientTest
             return doc.SelectSingleNode(path, nmgr);
         }
 
-        private static string GetRequest(string userName, string password, string ipcc, string domain)
+        private static string GetRequest(string userName, string password, string ipcc, string domain, string actionType)
         {
             return
                 "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:eb=\"http://www.ebxml.org/namespaces/messageHeader\">" +
@@ -80,7 +94,7 @@ namespace SabreClientTest
                             "<eb:From><eb:PartyId>9aq99999</eb:PartyId></eb:From>" +
                             "<eb:To><eb:PartyId>123ws123</eb:PartyId></eb:To>" +
                             "<eb:ConversationId>1234567</eb:ConversationId>" +
-                            "<eb:Action>SessionCreateRQ</eb:Action>" +
+                            $"<eb:Action>{actionType}</eb:Action>" +
                         "</eb:MessageHeader>" +
                         "<wsse:Security xmlns:wsse=\"http://schemas.xmlsoap.org/ws/2002/12/secext\">" +
                             "<wsse:UsernameToken>" +
@@ -93,49 +107,6 @@ namespace SabreClientTest
                     "</SOAP-ENV:Header>" +
                     "<SOAP-ENV:Body></SOAP-ENV:Body>" +
                 "</SOAP-ENV:Envelope>";
-
-            //$"<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:eb=\"http://www.ebxml.org/namespaces/messageHeader\">" +
-            //        "<SOAP-ENV:Header>" +
-            //            "<eb:MessageHeader SOAP-ENV:mustUnderstand=\"1\" eb:version=\"1.0\">" +
-            //                "<eb:From><eb:PartyId>9aq99999</eb:PartyId></eb:From>" +
-            //                "<eb:To><eb:PartyId>123ws123</eb:PartyId></eb:To>" +
-            //                "<eb:ConversationId>1234567</eb:ConversationId>" +
-            //                "<eb:Action>SessionCreateRQ</eb:Action>" +
-            //            "</eb:MessageHeader>" +
-            //            "<wsse:Security xmlns:wsse=\"http://schemas.xmlsoap.org/ws/2002/12/secext\">" +
-            //                "<wsse:UsernameToken>" +
-            //                    "<wsse:Username>7971</wsse:Username>" +
-            //                    "<wsse:Password>WS102513</wsse:Password>" +
-            //                    "<Organization>92RG</Organization>" +
-            //                    "<Domain>DEFAULT</Domain>" +
-            //                "</wsse:UsernameToken>" +
-            //            "</wsse:Security>" +
-            //        "</SOAP-ENV:Header>" +
-            //        "<SOAP-ENV:Body></SOAP-ENV:Body>" +
-            //    "</SOAP-ENV:Envelope>";
-
-            /*var body = new StringBuilder();
-            body.AppendLine("<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:eb=\"http://www.ebxml.org/namespaces/messageHeader\">");
-            body.AppendLine("<SOAP-ENV:Header>");
-            body.AppendLine("<eb:MessageHeader SOAP-ENV:mustUnderstand=\"1\" eb:version=\"1.0\">");
-            body.AppendLine("<eb:From><eb:PartyId>9aq99999</eb:PartyId></eb:From>");
-            body.AppendLine("<eb:To><eb:PartyId>123ws123</eb:PartyId></eb:To>");
-            body.AppendLine("<eb:ConversationId>1234567</eb:ConversationId>");
-            body.AppendLine("<eb:Action>SessionCreateRQ</eb:Action>");
-            body.AppendLine("</eb:MessageHeader>");
-            body.AppendLine("<wsse:Security xmlns:wsse=\"http://schemas.xmlsoap.org/ws/2002/12/secext\">");
-            body.AppendLine("<wsse:UsernameToken>");
-            body.AppendLine("<wsse:Username>7971</wsse:Username>");
-            body.AppendLine("<wsse:Password>WS102513</wsse:Password>");
-            body.AppendLine("<Organization>92RG</Organization>");
-            body.AppendLine("<Domain>DEFAULT</Domain>");
-            body.AppendLine("</wsse:UsernameToken>");
-            body.AppendLine("</wsse:Security>");
-            body.AppendLine("</SOAP-ENV:Header>");
-            body.AppendLine("<SOAP-ENV:Body></SOAP-ENV:Body>");
-            body.AppendLine("</SOAP-ENV:Envelope>");
-
-            return body.ToString();*/
         }
 
         private static string GetXmlResponse()
